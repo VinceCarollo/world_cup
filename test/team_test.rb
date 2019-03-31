5847eebb71a5ba8f2397f9270f93a06b5da0768a
Vince Carollo <vincecarollo@gmail.com>
2019-03-31 17:26:14 -0600

    Add team class and tests

---
+++ test/team_test.rb
@@ -0,0 +1,51 @@
+require 'minitest/autorun'
+require './lib/player'
+require './lib/team'
+
+
+class TeamTest < Minitest::Test
+  attr_reader :team,
+              :mbappe,
+              :pogba
+
+  def setup
+    @team = Team.new("France")
+    @mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    @pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+  end
+
+  def test_it_exists
+    assert_instance_of Team, team
+  end
+
+  def test_it_has_attributes
+    assert_equal "France", team.country
+  end
+
+  def test_it_can_be_eliminated
+    refute team.eliminated?
+
+    team.eliminated = true
+
+    assert team.eliminated?
+  end
+
+  def test_it_starts_with_no_players
+    assert_equal [], team.players
+  end
+
+  def test_it_can_add_players
+    team.add(mbappe)
+    team.add(pogba)
+
+    assert_equal [mbappe, pogba], team.players
+  end
+
+  def test_it_can_get_players_by_position
+    team.add(mbappe)
+    team.add(pogba)
+
+    assert_equal [pogba], team.players_by_position("midfielder")
+    assert_equal [], team.players_by_position("defender")
+  end
+end
